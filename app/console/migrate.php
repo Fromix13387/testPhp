649382009b490c099c282090fa3cd59300c22b45
Fromix <ivchenko1000209@gmail.com>
2026-08-27 11:24:32 +0700

    fix: path migrate.php

--- app/console/migrate.php
+++ app/console/migrate.php
@@ -2,9 +2,9 @@
 
 use app\core\Database;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-$config = require __DIR__ . '/config/config.php';
+$config = require __DIR__ . '/../config/config.php';
 
 $db = new Database($config['components']['db']);
 
